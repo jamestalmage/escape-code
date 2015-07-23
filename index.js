cd7370ba917763b53b41e9a2afcbbd87ca9e1c51
James Talmage <james@talmage.io>
2015-07-23 02:26:22 -0400

    use stringish

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
 module.exports = EscapeCode;
-var Delegator = require('delegates');
+var stringish = require('stringish');
 
 function EscapeCode(str) {
   if (!(this instanceof EscapeCode)) {
     return new EscapeCode(str);
   }
-  String.call(this, str);
   Object.defineProperty(this, '_stringValue', {
     value: str
   });
@@ -14,25 +13,10 @@ function EscapeCode(str) {
 
 var ecp = EscapeCode.prototype;
 
+stringish(ecp);
+
 ecp.isEscapeCode = true;
 
 ecp.toString = ecp.valueOf = function() {
   return this._stringValue;
 };
-
-new Delegator(ecp, '_stringValue')
-  .method('charAt')
-  .method('charCodeAt')
-  .method('concat')
-  .method('indexOf')
-  .method('lastIndexOf')
-  .method('match')
-  .method('replace')
-  .method('search')
-  .method('slice')
-  .method('split')
-  .method('substr')
-  .method('substring')
-  .method('toLowerCase')
-  .method('toUpperCase')
-  .method('trim');
--- test/test.js
+++ test/test.js
@@ -4,38 +4,11 @@ var EscapeCode = require('../');
 var escapeCode = EscapeCode;
 describe('escape-code', function() {
 
-  it('toString should equal input', function() {
-    assert.equal(
-      new EscapeCode('\u001b[3m').toString(),
-      '\u001b[3m'
-    );
-  });
-
-  it('valueOf should equal input', function() {
-    assert.equal(
-      new EscapeCode('\u001b[3m').valueOf(),
-      '\u001b[3m'
-    );
-  });
-
-  it('comparison via assert.equal', function() {
-    assert.equal(
-      new EscapeCode('\u001b[3m'),
-      '\u001b[3m'
-    );
-  });
-
   it('will handle use without `new` keyword', function() {
     assert.equal(
       escapeCode('\u001b[3m').valueOf(),
       '\u001b[3m'
     );
-  });
-
-  it('use with regex', function() {
-    assert(/^\u001b/.test(new EscapeCode('\u001b')));
-
-    assert.equal('\u001b', /\u001b/.exec('\u001b')[0]);
   });
 
   it('has isEscapeCode property === true', function() {
@@ -51,19 +24,6 @@ describe('escape-code', function() {
 
     assert.strictEqual(s.charAt(1), e.charAt(1), 'charAt');
     assert.strictEqual(s.charCodeAt(2), e.charCodeAt(2), 'charCodeAt');
-    assert.strictEqual(s.concat('abc'), e.concat('abc'), 'concat');
-    assert.strictEqual(s.indexOf('3'), e.indexOf('3'), 'indexOf');
-    assert.strictEqual(s.lastIndexOf('3'), e.lastIndexOf('3'), 'lastIndexOf');
-    assert.deepEqual(s.match(/(\d)(\d)/), e.match(/(\d)(\d)/), 'match');
-    assert.strictEqual(s.replace('33', '43'), e.replace('33', '43'), 'replace');
-    assert.strictEqual(s.search('33'), e.search('33'), 'search');
-    assert.strictEqual(s.slice(2), e.slice(2), 'slice');
-    assert.deepEqual(s.split('3'), e.split('3'), 'split');
-    assert.strictEqual(s.substr(2), e.substr(2), 'substr');
-    assert.strictEqual(s.substring(3), e.substring(3), 'substring');
-    assert.strictEqual(s.toLowerCase(), e.toLowerCase(), 'toLowerCase');
-    assert.strictEqual(s.toUpperCase(), e.toUpperCase(), 'toUpperCase');
-    assert.strictEqual(s.trim(), e.trim(), 'trim');
   });
 
   it('JSON is the same', function() {
